42b21c8b4bb393160a8f3905f75a4d76bd811a44
abdulShukor <ashukoor12@gmail.com>
2019-03-31 00:41:27 -0400

    Modified and added to Tom's Code,Angular CRUD,Angular 5 Pages, Angular Security

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-   "URI" : "mongodb://localhost/test",
-    //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+   //"URI" : "mongodb://localhost/test",
+    "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
     //"URI": "mongodb://abdul:L123456@ds050739.mlab.com:50739/comp308-mongodb"
 
     "secret": "SomeSecret"
